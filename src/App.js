1c91ba5ef1097e6f66112897a5e0dbef07f228f0
logesh sundar <logesh2410@gmail.com>
2022-01-23 17:09:09 +0530

    adding Header Nav Menu

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import './components/header/header.js'
+import HeaderNav from './components/header/header.js';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <HeaderNav />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
---
+++ src/components/header/header.js
@@ -0,0 +1,5 @@
+function HeaderNav() {
+    return <h2>Hi, I am a Header Menu!</h2>;
+  }
+  
+  export default HeaderNav;
